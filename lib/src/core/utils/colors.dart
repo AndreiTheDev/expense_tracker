13194f80c903e286ec1b3f6f0e2da6d73d684fd8
AndreiTheDev <andreithedevwork@gmail.com>
2024-02-12 14:44:37 +0200

    feat: create utils folder and colors and sizes utilities

---
+++ lib/src/core/utils/colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const Color backgroundColor = Color(0xfff3f4f6);
+const Color textBlue = Color(0xff43648f);
+const Color inactiveColor = Color(0xffc1cadb);
+const Color textDark = Color(0xff354366);
+const LinearGradient buttonsGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [
+    Color(0xff4998d3),
+    Color(0xff9290e5),
+    Color(0xffca6ce5),
+    Color(0xffe686b3),
+    Color(0xfff9b15f),
+  ],
+);
+const LinearGradient buttonsGradientReversed = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [
+    Color(0xfff9b15f),
+    Color(0xffe686b3),
+    Color(0xffca6ce5),
+    Color(0xff9290e5),
+    Color(0xff4998d3),
+  ],
+);
---
+++ lib/src/core/utils/sizes.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const double xsSize = 8;
+const double smallSize = 16;
+const double mediumSize = 24;
+const double largeSize = 36;
+const double xlSize = 48;
+
+const double textSmall = 14;
+const double textMedium = 24;
+const double textLarge = 36;
+const double textXl = 48;
+
+const xsSeparator = SizedBox(
+  height: xsSize,
+);
+const smallSeparator = SizedBox(
+  height: smallSize,
+);
+const mediumSeparator = SizedBox(
+  height: mediumSize,
+);
+const largeSeparator = SizedBox(
+  height: largeSize,
+);
+const xlSeparator = SizedBox(
+  height: xlSize,
+);
