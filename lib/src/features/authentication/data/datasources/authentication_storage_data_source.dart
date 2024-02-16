baaf01f291a5e81e0daa952ab76249fb0d68746d
AndreiTheDev <andreithedevwork@gmail.com>
2024-02-16 23:18:18 +0200

    feat: add function inside storage data source to fetch all profile pictures urls

--- lib/src/features/authentication/data/datasources/authentication_storage_data_source.dart
+++ lib/src/features/authentication/data/datasources/authentication_storage_data_source.dart
@@ -6,6 +6,7 @@ abstract interface class AuthenticationStorageDataSource {
   Future<String> addPhotoToStorage(
     final File photo,
   );
+  Future<List<String>> fetchProfilePhotosUrls();
 }
 
 class AuthenticationStorageDataSourceImpl
@@ -15,6 +16,8 @@ class AuthenticationStorageDataSourceImpl
 
   AuthenticationStorageDataSourceImpl(this._storageInstance);
 
+  //not to be used for the moment,
+  //we will let users only pick from our profile pics
   @override
   Future<String> addPhotoToStorage(
     final File photo,
@@ -24,5 +27,17 @@ class AuthenticationStorageDataSourceImpl
     final taskSnapshot = await uploadTask;
     final photoUrl = await taskSnapshot.ref.getDownloadURL();
     return photoUrl;
+  }
+
+  @override
+  Future<List<String>> fetchProfilePhotosUrls() async {
+    final List<String> photosUrls = [];
+    final refrenceList =
+        await FirebaseStorage.instance.ref().child('profile-photos').listAll();
+    for (final Reference file in refrenceList.items) {
+      final url = await file.getDownloadURL();
+      photosUrls.add(url);
+    }
+    return photosUrls;
   }
 }
