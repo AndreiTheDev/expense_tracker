c14d458b3f14b09e19d8c98e9da2a797a1b0e04e
AndreiTheDev <andreithedevwork@gmail.com>
2024-02-16 23:16:50 +0200

    feat: implemented new repository to fetch profile pictures urls and tested it

---
+++ test/src/features/authentication/data/repositories/profile_photos_repository_impl_test.dart
@@ -0,0 +1,55 @@
+import 'package:expense_tracker_app_bloc/src/core/error/failures.dart';
+import 'package:expense_tracker_app_bloc/src/features/authentication/data/datasources/authentication_storage_data_source.dart';
+import 'package:expense_tracker_app_bloc/src/features/authentication/data/repositories/profile_photos_repository_impl.dart';
+import 'package:expense_tracker_app_bloc/src/features/authentication/domain/repositories/profile_photos_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'profile_photos_repository_impl_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<AuthenticationStorageDataSourceImpl>()])
+void main() {
+  late MockAuthenticationStorageDataSourceImpl mockStorageDataSource;
+  late ProfilePhotosRepository sut;
+
+  setUp(() {
+    mockStorageDataSource = MockAuthenticationStorageDataSourceImpl();
+    sut = ProfilePhotosRepositoryImpl(storageDataSource: mockStorageDataSource);
+  });
+
+  test('Fetch profile photos urls is successful', () async {
+    const resultArray = [
+      'testurl',
+      'testurl2',
+      'testurl3',
+    ];
+    when(mockStorageDataSource.fetchProfilePhotosUrls()).thenAnswer(
+      (realInvocation) async => resultArray,
+    );
+
+    final response = await sut.fetchProfilePhotosUrls();
+
+    expect(
+      response,
+      const Right(resultArray),
+    );
+    verify(mockStorageDataSource.fetchProfilePhotosUrls()).called(1);
+    verifyNoMoreInteractions(mockStorageDataSource);
+  });
+
+  test('Fetch profile photos urls fails', () async {
+    when(mockStorageDataSource.fetchProfilePhotosUrls())
+        .thenThrow(Exception('An unknown error occured.'));
+
+    final response = await sut.fetchProfilePhotosUrls();
+
+    expect(
+      response,
+      const Left(AuthFailure(message: 'An unknown error occured.')),
+    );
+    verify(mockStorageDataSource.fetchProfilePhotosUrls()).called(1);
+    verifyNoMoreInteractions(mockStorageDataSource);
+  });
+}
