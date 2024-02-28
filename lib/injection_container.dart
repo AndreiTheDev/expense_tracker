0845f7f26954b074d8fa41ac0bb17cccf0fc90de
AndreiTheDev <andreithedevwork@gmail.com>
2024-02-28 10:00:25 +0200

    feat: add the new dependencies to the service locator

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,12 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get_it/get_it.dart';
 
 import 'src/features/authentication/data/datasources/authentication_firebase_data_source.dart';
 import 'src/features/authentication/data/datasources/authentication_firestore_data_source.dart';
-import 'src/features/authentication/data/datasources/authentication_functions_data_source.dart';
+import 'src/features/authentication/data/datasources/authentication_messaging_data_source.dart';
 import 'src/features/authentication/data/datasources/authentication_storage_data_source.dart';
 import 'src/features/authentication/data/repositories/authentication_repository_impl.dart';
 import 'src/features/authentication/data/repositories/profile_photos_repository_impl.dart';
@@ -23,6 +24,7 @@ import 'src/features/authentication/presentation/blocs/user_bloc/user_bloc.dart'
 import 'src/features/authentication/presentation/cubits/profile_photos_urls/profile_photos_urls_cubit.dart';
 import 'src/features/authentication/presentation/cubits/recover_password_form/recover_password_form_cubit.dart';
 import 'src/features/authentication/presentation/cubits/sign_in_form/sign_in_form_cubit.dart';
+import 'src/features/authentication/presentation/cubits/sign_up_form/sign_up_form_cubit.dart';
 
 final sl = GetIt.instance;
 
@@ -44,6 +46,7 @@ Future<void> init() async {
     ..registerFactory(() => ProfilePhotosUrlsCubit(profilePhotosUrls: sl()))
     ..registerFactory(SignInFormCubit.new)
     ..registerFactory(RecoverPasswordFormCubit.new)
+    ..registerFactory(SignUpFormCubit.new)
 
     //Use Cases
     ..registerLazySingleton(() => DeleteUser(sl()))
@@ -59,8 +62,8 @@ Future<void> init() async {
       () => AuthenticationRepositoryImpl(
         firebaseDataSource: sl(),
         firestoreDataSource: sl(),
-        functionsDataSource: sl(),
         storageDataSource: sl(),
+        messagingDataSource: sl(),
       ),
     )
     ..registerLazySingleton<ProfilePhotosRepository>(
@@ -74,16 +77,17 @@ Future<void> init() async {
     ..registerLazySingleton<AuthenticationFirestoreDataSource>(
       () => AuthenticationFirestoreDataSourceImpl(sl()),
     )
-    ..registerLazySingleton<AuthenticationFunctionsDataSource>(
-      () => AuthenticationFunctionsDataSourceImpl(sl()),
-    )
     ..registerLazySingleton<AuthenticationStorageDataSource>(
       () => AuthenticationStorageDataSourceImpl(sl()),
+    )
+    ..registerLazySingleton<AuthenticationMessagingDataSource>(
+      () => AuthenticationMessagingDataSourceImpl(sl())..init(),
     )
 
     //Firebase instances
     ..registerLazySingleton(() => FirebaseAuth.instance)
     ..registerLazySingleton(() => FirebaseFirestore.instance)
     ..registerLazySingleton(() => FirebaseFunctions.instance)
+    ..registerLazySingleton(() => FirebaseMessaging.instance)
     ..registerLazySingleton(() => FirebaseStorage.instance);
 }
