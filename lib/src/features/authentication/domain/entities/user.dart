cd4a9207fb34bf8516ad4d12750fc14ee8f89fd9
AndreiTheDev <andreithedevwork@gmail.com>
2024-02-28 09:39:05 +0200

    fix: make fcm nullable in case user denies notifications permission

--- lib/src/features/authentication/domain/entities/user.dart
+++ lib/src/features/authentication/domain/entities/user.dart
@@ -4,7 +4,7 @@ import 'package:equatable/equatable.dart';
 class UserEntity extends Equatable {
   final String uid;
   final String email;
-  final String fcmToken;
+  final String? fcmToken;
   final String completeName;
   final String photoUrl;
 
