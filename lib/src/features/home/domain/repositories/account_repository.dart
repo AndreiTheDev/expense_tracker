30c86541fef3e3c32216ab31dacb6332d97d0c8b
AndreiTheDev <andreithedevwork@gmail.com>
2024-03-16 21:00:22 +0200

    feat: added new functions to the account repository

--- lib/src/features/home/domain/repositories/account_repository.dart
+++ lib/src/features/home/domain/repositories/account_repository.dart
@@ -2,18 +2,31 @@ import 'package:fpdart/fpdart.dart';
 
 import '../../../../core/error/failures.dart';
 import '../entities/account.dart';
-import '../entities/transaction.dart';
+import '../entities/expense.dart';
+import '../entities/income.dart';
 
 abstract interface class AccountRepository {
   Future<Either<Failure, AccountEntity>> fetchAccount({
     final String id = 'defaultAccount',
   });
 
-  Future<Either<Failure, void>> addTransaction({
-    required final TransactionEntity entity,
+  Future<Either<Failure, void>> addExpense({
+    required final ExpenseEntity expenseEntity,
+  });
+
+  Future<Either<Failure, void>> addIncome({
+    required final IncomeEntity incomeEntity,
+  });
+
+  Future<Either<Failure, void>> deleteExpense({
+    required final String expenseId,
+  });
+
+  Future<Either<Failure, void>> deleteIncome({
+    required final String incomeId,
   });
 
   Future<Either<Failure, void>> deleteTransaction({
-    required final String id,
+    required final String transactionId,
   });
 }
