9d221765b802dd036314217f671fc290d0d34f57
AndreiTheDev <andreithedevwork@gmail.com>
2024-03-19 21:43:26 +0200

    feat: implement account bloc tests remaining to be written

--- lib/src/features/home/presentation/bloc/account_event.dart
+++ lib/src/features/home/presentation/bloc/account_event.dart
@@ -7,10 +7,59 @@ sealed class AccountEvent extends Equatable {
   List<Object> get props => [];
 }
 
-class AccountFetchEvent extends AccountEvent {}
+class AccountFetchEvent extends AccountEvent {
+  final String accountId;
 
-class AccountAddIncomeEvent extends AccountEvent {}
+  const AccountFetchEvent({this.accountId = 'default'});
 
-class AccountAddExpenseEvent extends AccountEvent {}
+  @override
+  List<Object> get props => [accountId];
+}
 
-class AccountDeleteTransactionEvent extends AccountEvent {}
+class AccountAddIncomeEvent extends AccountEvent {
+  final String accountId;
+  final IncomeEntity incomeEntity;
+
+  const AccountAddIncomeEvent({
+    required this.incomeEntity,
+    this.accountId = 'default',
+  });
+
+  @override
+  List<Object> get props => [incomeEntity, accountId];
+}
+
+class AccountAddExpenseEvent extends AccountEvent {
+  final String accountId;
+  final ExpenseEntity expenseEntity;
+
+  const AccountAddExpenseEvent({
+    required this.expenseEntity,
+    this.accountId = 'default',
+  });
+
+  @override
+  List<Object> get props => [accountId, expenseEntity];
+}
+
+class AccountDeleteTransactionEvent extends AccountEvent {
+  final String accountId;
+  final TransactionEntity transactionEntity;
+
+  const AccountDeleteTransactionEvent({
+    required this.transactionEntity,
+    this.accountId = 'default',
+  });
+
+  @override
+  List<Object> get props => [accountId, transactionEntity];
+}
+
+class AccountErrorEvent extends AccountEvent {
+  final String message;
+
+  const AccountErrorEvent({required this.message});
+
+  @override
+  List<Object> get props => [message];
+}
--- lib/src/features/home/presentation/bloc/account_state.dart
+++ lib/src/features/home/presentation/bloc/account_state.dart
@@ -11,6 +11,13 @@ final class AccountInitial extends AccountState {}
 
 final class AccountLoading extends AccountState {}
 
-final class AccountLoaded extends AccountState {}
+final class AccountLoaded extends AccountState {
+  final AccountEntity account;
+
+  const AccountLoaded(this.account);
+
+  @override
+  List<Object> get props => [account];
+}
 
 final class AccountError extends AccountState {}
