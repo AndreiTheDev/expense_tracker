ee24457b26df2331cba28a53a26408e12462120f
AndreiTheDev <andreithedevwork@gmail.com>
2024-03-20 23:46:36 +0200

    feat: add loading view and placeholder error view and also implement a good data flow

--- lib/src/features/home/presentation/views/home_view.dart
+++ lib/src/features/home/presentation/views/home_view.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 import '../../../../../injection_container.dart';
-import '../../../../core/common_widgets/custom_appbar_button.dart';
 import '../../../../core/routing/app_routes.dart';
 import '../../../../core/utils/utils.dart';
-import '../../../authentication/presentation/blocs/user_bloc/user_bloc.dart';
 import '../bloc/account_bloc.dart';
+import '../widgets/home_app_bar.dart';
 import '../widgets/home_bottom_navigation_bar.dart';
 import '../widgets/home_card.dart';
 import '../widgets/home_transactions_listview.dart';
@@ -23,123 +23,63 @@ class HomeView extends StatelessWidget {
       child: Builder(
         builder: (context) {
           return Scaffold(
-            body: RefreshIndicator(
-              onRefresh: () {
-                return Future(
-                  () => context
-                      .read<AccountBloc>()
-                      .add(const AccountFetchEvent()),
-                );
-              },
-              child: SingleChildScrollView(
-                physics: const ClampingScrollPhysics(),
-                child: SizedBox(
-                  height: MediaQuery.of(context).size.height - appBarHeight + 1,
-                  child: Column(
+            body: BlocBuilder<AccountBloc, AccountState>(
+              builder: (context, state) {
+                if (state is AccountLoaded) {
+                  final currentAccount = state.account;
+                  return Column(
                     children: [
                       xlSeparator,
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          left: mediumSize,
-                          right: mediumSize,
-                          top: mediumSize,
-                        ),
-                        child: Row(
-                          children: [
-                            BlocBuilder<UserBloc, UserState>(
-                              builder: (context, state) {
-                                if (state is UserAuthenticated) {
-                                  return CircleAvatar(
-                                    backgroundImage: NetworkImage(
-                                      state.user.photoUrl,
-                                    ),
-                                  );
-                                }
-                                return const CircleAvatar(
-                                  backgroundColor: Colors.amber,
-                                );
-                              },
-                            ),
-                            smallSeparator,
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  'Welcome!',
-                                  style: TextStyle(
-                                    fontSize: smallText,
-                                  ),
-                                ),
-                                BlocBuilder<UserBloc, UserState>(
-                                  builder: (context, state) {
-                                    if (state is UserAuthenticated) {
-                                      return Text(
-                                        state.user.completeName,
-                                        style: const TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: textDark,
-                                        ),
-                                      );
-                                    }
-                                    return const Text(
-                                      'John Doe',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: textDark,
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ],
-                            ),
-                            const Expanded(child: SizedBox()),
-                            CustomAppbarButton(
-                              onTap: () {},
-                              icon: Icons.mode_standby,
-                            ),
-                          ],
-                        ),
-                      ),
+                      const HomeAppBar(),
                       smallSeparator,
-                      const HomeCard(),
+                      HomeCard(
+                        currentAccount: currentAccount,
+                      ),
                       mediumSeparator,
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          left: mediumSize,
-                          right: mediumSize,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Text(
-                              'Transactions',
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            TextButton(
-                              onPressed: () {},
-                              style: ButtonStyle(
-                                overlayColor: MaterialStatePropertyAll(
-                                  textDark.withOpacity(0.15),
-                                ),
-                              ),
-                              child: const Text(
-                                'View All',
-                                style: TextStyle(
-                                  color: textDark,
-                                ),
-                              ),
-                            ),
-                          ],
+                      const TransactionsListHeader(),
+                      HomeTransactionsListview(
+                        transactionsList: currentAccount.transactions,
+                      ),
+                    ],
+                  );
+                }
+                if (state is AccountLoading) {
+                  return const Column(
+                    children: [
+                      xlSeparator,
+                      HomeAppBarLoading(),
+                      smallSeparator,
+                      HomeCardLoading(),
+                      mediumSeparator,
+                      Skeletonizer(
+                        child: TransactionsListHeader(),
+                      ),
+                      HomeTransactionsListviewLoading(),
+                    ],
+                  );
+                }
+                return RefreshIndicator(
+                  onRefresh: () {
+                    return Future(
+                      () => context
+                          .read<AccountBloc>()
+                          .add(const AccountFetchEvent()),
+                    );
+                  },
+                  child: SingleChildScrollView(
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height,
+                      child: const Center(
+                        child: Text(
+                          'An error occured, please refresh the page.',
                         ),
                       ),
-                      const HomeTransactionsListview(),
-                    ],
+                    ),
                   ),
-                ),
-              ),
+                );
+              },
             ),
             floatingActionButtonLocation:
                 FloatingActionButtonLocation.centerDocked,
