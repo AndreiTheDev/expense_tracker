0062e0cbe63bca64d97a4d858e74be8607cea83d
AndreiTheDev <andreithedevwork@gmail.com>
2024-03-21 21:20:27 +0200

    feat: move AccountBloc provider to the main app so it can be accessible inside all screens

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -5,14 +5,22 @@ import '../injection_container.dart';
 import 'core/routing/app_router.dart';
 import 'core/theme/theme.dart';
 import 'features/authentication/presentation/blocs/user_bloc/user_bloc.dart';
+import 'features/home/presentation/bloc/account_bloc.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => sl<UserBloc>()..add(UserStarted()),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => sl<UserBloc>()..add(UserStarted()),
+        ),
+        BlocProvider(
+          create: (context) => sl<AccountBloc>(),
+        ),
+      ],
       child: BlocListener<UserBloc, UserState>(
         listener: (context, state) {
           if (state is UserAuthenticated || state is UserUnauthenticated) {
