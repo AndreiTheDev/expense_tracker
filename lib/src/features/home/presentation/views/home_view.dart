8d430d9fd71db5e251422cefb96b3101ece8f76c
AndreiTheDev <andreithedevwork@gmail.com>
2024-03-21 21:26:58 +0200

    fix: removed Account Bloc provider and add call to fetch account on load

--- lib/src/features/home/presentation/views/home_view.dart
+++ lib/src/features/home/presentation/views/home_view.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
-import '../../../../../injection_container.dart';
 import '../../../../core/routing/app_routes.dart';
 import '../../../../core/utils/utils.dart';
 import '../bloc/account_bloc.dart';
@@ -18,95 +17,94 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const double appBarHeight = 90;
-    return BlocProvider<AccountBloc>(
-      create: (context) => sl()..add(const AccountFetchEvent()),
-      child: Builder(
-        builder: (context) {
-          return Scaffold(
-            body: BlocBuilder<AccountBloc, AccountState>(
-              builder: (context, state) {
-                if (state is AccountLoaded) {
-                  final currentAccount = state.account;
-                  return Column(
-                    children: [
-                      xlSeparator,
-                      const HomeAppBar(),
-                      smallSeparator,
-                      HomeCard(
-                        currentAccount: currentAccount,
-                      ),
-                      mediumSeparator,
-                      const TransactionsListHeader(),
-                      HomeTransactionsListview(
-                        transactionsList: currentAccount.transactions,
-                      ),
-                    ],
+    context.read<AccountBloc>().add(const AccountFetchEvent());
+
+    return Builder(
+      builder: (context) {
+        return Scaffold(
+          body: BlocBuilder<AccountBloc, AccountState>(
+            builder: (context, state) {
+              if (state is AccountLoaded) {
+                final currentAccount = state.account;
+                return Column(
+                  children: [
+                    xlSeparator,
+                    const HomeAppBar(),
+                    smallSeparator,
+                    HomeCard(
+                      currentAccount: currentAccount,
+                    ),
+                    mediumSeparator,
+                    const TransactionsListHeader(),
+                    HomeTransactionsListview(
+                      transactionsList: currentAccount.transactions,
+                    ),
+                  ],
+                );
+              }
+              if (state is AccountLoading) {
+                return const Column(
+                  children: [
+                    xlSeparator,
+                    HomeAppBarLoading(),
+                    smallSeparator,
+                    HomeCardLoading(),
+                    mediumSeparator,
+                    Skeletonizer(
+                      child: TransactionsListHeader(),
+                    ),
+                    HomeTransactionsListviewLoading(),
+                  ],
+                );
+              }
+              return RefreshIndicator(
+                onRefresh: () {
+                  return Future(
+                    () => context
+                        .read<AccountBloc>()
+                        .add(const AccountFetchEvent()),
                   );
-                }
-                if (state is AccountLoading) {
-                  return const Column(
-                    children: [
-                      xlSeparator,
-                      HomeAppBarLoading(),
-                      smallSeparator,
-                      HomeCardLoading(),
-                      mediumSeparator,
-                      Skeletonizer(
-                        child: TransactionsListHeader(),
-                      ),
-                      HomeTransactionsListviewLoading(),
-                    ],
-                  );
-                }
-                return RefreshIndicator(
-                  onRefresh: () {
-                    return Future(
-                      () => context
-                          .read<AccountBloc>()
-                          .add(const AccountFetchEvent()),
-                    );
-                  },
-                  child: SingleChildScrollView(
-                    physics: const AlwaysScrollableScrollPhysics(),
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height,
-                      child: const Center(
-                        child: Text(
-                          'An error occured, please refresh the page.',
-                        ),
+                },
+                child: SingleChildScrollView(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: const Center(
+                      child: Text(
+                        'An error occured, please refresh the page.',
                       ),
                     ),
                   ),
-                );
-              },
-            ),
-            floatingActionButtonLocation:
-                FloatingActionButtonLocation.centerDocked,
-            floatingActionButton: FloatingActionButton(
-              onPressed: () async {
-                await context.pushNamed(AppRoutes.addTransactions.name);
-              },
-              shape: const CircleBorder(),
-              child: Container(
-                height: double.maxFinite,
-                width: double.maxFinite,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: buttonsGradientReversed,
                 ),
-                child: const Icon(
-                  Icons.add,
-                  size: largeSize,
-                ),
+              );
+            },
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () async {
+              await context.pushNamed(AppRoutes.addTransactions.name);
+            },
+            shape: const CircleBorder(),
+            child: Container(
+              height: double.maxFinite,
+              width: double.maxFinite,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: buttonsGradientReversed,
+              ),
+              child: const Icon(
+                Icons.add,
+                size: largeSize,
               ),
             ),
-            bottomNavigationBar: const HomeBottomNavigationBar(
-              appBarHeight: appBarHeight,
-            ),
-          );
-        },
-      ),
+          ),
+          bottomNavigationBar: const HomeBottomNavigationBar(
+            appBarHeight: appBarHeight,
+          ),
+        );
+      },
     );
   }
 }
