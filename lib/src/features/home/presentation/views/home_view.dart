a5209ac188312c0012b9394d3a21c2b6f7df7602
AndreiTheDev <andreithedevwork@gmail.com>
2024-04-03 23:46:52 +0300

    fix: add the newly renamed transactions list loading widget

--- lib/src/features/home/presentation/views/home_view.dart
+++ lib/src/features/home/presentation/views/home_view.dart
@@ -59,7 +59,7 @@ class HomeView extends StatelessWidget {
                     Skeletonizer(
                       child: TransactionsListHeader(),
                     ),
-                    HomeTransactionsListviewLoading(),
+                    TransactionsListviewLoading(),
                   ],
                 );
               }
