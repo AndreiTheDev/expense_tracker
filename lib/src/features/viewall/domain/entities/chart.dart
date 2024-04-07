39aa39d3f1d3d3fcc8223fb3c105a03acf3060ff
AndreiTheDev <andreithedevwork@gmail.com>
2024-04-07 22:20:30 +0300

    feat: create monthly_chart_data_entity and add implementation to chart_entity

--- lib/src/features/viewall/domain/entities/chart.dart
+++ lib/src/features/viewall/domain/entities/chart.dart
@@ -1,6 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 
+import 'monthly_chart_data.dart';
+
 class ChartEntity extends Equatable {
+  final List<MonthlyChartDataEntity> monthlyList;
+
+  const ChartEntity({
+    required this.monthlyList,
+  });
+
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [monthlyList];
 }
