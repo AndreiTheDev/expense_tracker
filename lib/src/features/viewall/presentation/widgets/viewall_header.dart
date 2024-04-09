75559f96716c84df8fcc24b3dc7607fe5e0794a3
AndreiTheDev <andreithedevwork@gmail.com>
2024-04-09 21:36:17 +0300

    feat: implement viewallheader widget so the view will be easier to read

---
+++ lib/src/features/viewall/presentation/widgets/viewall_header.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../core/common_widgets/custom_appbar.dart';
+import '../../../../core/common_widgets/custom_appbar_button.dart';
+import '../../../../core/common_widgets/custom_switcher.dart';
+import '../../../../core/utils/utils.dart';
+import '../../../home/presentation/bloc/account_bloc.dart';
+import '../cubit/viewall_view_cubit.dart';
+
+class ViewallHeader extends StatelessWidget {
+  const ViewallHeader({
+    required this.isIos,
+    required this.isIncomesActive,
+    super.key,
+  });
+
+  final bool isIos;
+  final bool isIncomesActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        xlSeparator,
+        Padding(
+          padding: const EdgeInsets.only(
+            left: mediumSize,
+            top: mediumSize,
+            right: mediumSize,
+          ),
+          child: CustomAppbar(
+            leftButton: CustomAppbarButton(
+              icon: isIos ? Icons.arrow_back_ios_new : Icons.arrow_back,
+              onTap: () {
+                context.read<AccountBloc>().add(const AccountFetchEvent());
+                context.pop();
+              },
+            ),
+            middleWidget: const Text(
+              'Transactions',
+              style: TextStyle(
+                fontSize: mediumText,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            rightButton: CustomAppbarButton(
+              icon: Icons.tune,
+              onTap: () {},
+            ),
+          ),
+        ),
+        mediumSeparator,
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: mediumSize,
+          ),
+          child: CustomSwitcher(
+            isFirstButtonActive: isIncomesActive,
+            firstButtonOnTap: () {
+              context.read<ViewallViewCubit>().switchToIncomes();
+            },
+            firstButtonText: 'Incomes',
+            secondButtonOnTap: () {
+              context.read<ViewallViewCubit>().switchToExpenses();
+            },
+            secondButtonText: 'Expenses',
+          ),
+        ),
+      ],
+    );
+  }
+}
