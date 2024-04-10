742d379b64fc2f3c4bd33035295d6c624ae61fa5
AndreiTheDev <andreithedevwork@gmail.com>
2024-04-10 22:08:55 +0300

    feat: implement viewall view

--- lib/src/features/viewall/presentation/views/viewall_view.dart
+++ lib/src/features/viewall/presentation/views/viewall_view.dart
@@ -1,26 +1,19 @@
+// ignore_for_file: lines_longer_than_80_chars
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../../../../injection_container.dart';
-import '../../../../core/common_widgets/custom_appbar.dart';
-import '../../../../core/common_widgets/custom_appbar_button.dart';
-import '../../../../core/common_widgets/custom_switcher.dart';
 import '../../../../core/common_widgets/transactions_listview.dart';
 import '../../../../core/utils/utils.dart';
-import '../../../home/presentation/bloc/account_bloc.dart';
 import '../blocs/viewall_bloc.dart';
 import '../cubit/viewall_view_cubit.dart';
 import '../widgets/viewall_chart.dart';
+import '../widgets/viewall_header.dart';
 
-class ViewallView extends StatefulWidget {
+class ViewallView extends StatelessWidget {
   const ViewallView({super.key});
 
-  @override
-  State<ViewallView> createState() => _ViewallViewState();
-}
-
-class _ViewallViewState extends State<ViewallView> {
   @override
   Widget build(BuildContext context) {
     final isIos = Theme.of(context).platform == TargetPlatform.iOS;
@@ -53,131 +46,174 @@ class _ViewallViewState extends State<ViewallView> {
                 }
               },
               builder: (context, viewState) {
-                return Column(
-                  children: [
-                    xlSeparator,
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: mediumSize,
-                        top: mediumSize,
-                        right: mediumSize,
-                      ),
-                      child: CustomAppbar(
-                        leftButton: CustomAppbarButton(
-                          icon: isIos
-                              ? Icons.arrow_back_ios_new
-                              : Icons.arrow_back,
-                          onTap: () {
-                            context
-                                .read<AccountBloc>()
-                                .add(const AccountFetchEvent());
-                            context.pop();
-                          },
+                switch (viewState) {
+                  case ViewallViewIncomes():
+                    return Column(
+                      children: [
+                        ViewallHeader(
+                          isIos: isIos,
+                          isIncomesActive: true,
                         ),
-                        middleWidget: const Text(
-                          'Transactions',
-                          style: TextStyle(
-                            fontSize: mediumText,
-                            fontWeight: FontWeight.bold,
+                        mediumSeparator,
+                        Expanded(
+                          child: BlocBuilder<ViewallBloc, ViewallState>(
+                            builder: (context, blocState) {
+                              switch (blocState) {
+                                case ViewallLoaded()
+                                    when blocState.incomesDetails != null:
+                                  return Column(
+                                    children: [
+                                      ViewallChart(
+                                        chart: blocState
+                                            .incomesDetails!.incomesChart,
+                                      ),
+                                      smallSeparator,
+                                      if (blocState.incomesDetails!.incomesList
+                                          .isNotEmpty)
+                                        TransactionsListview(
+                                          transactionsListCards: [
+                                            for (final incomeEntity in blocState
+                                                .incomesDetails!.incomesList)
+                                              TransactionsListCard(
+                                                transaction: incomeEntity,
+                                                deleteTransactionCallback: () {
+                                                  context
+                                                      .read<ViewallBloc>()
+                                                      .add(
+                                                        ViewallDeleteIncomeEvent(
+                                                          incomeEntity:
+                                                              incomeEntity,
+                                                        ),
+                                                      );
+                                                },
+                                              ),
+                                          ],
+                                        )
+                                      else
+                                        const Padding(
+                                          padding:
+                                              EdgeInsets.only(top: smallSize),
+                                          child:
+                                              Text('There are no incomes yet.'),
+                                        ),
+                                    ],
+                                  );
+                                case ViewallLoaded()
+                                    when blocState.incomesDetails == null:
+                                  return const Padding(
+                                    padding: EdgeInsets.all(mediumSize),
+                                    child: Text(
+                                      'Unable to fetch data. Please reload the page.',
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  );
+                                case ViewallLoading():
+                                  return const Column(
+                                    children: [
+                                      ViewallChartLoading(),
+                                      smallSeparator,
+                                      TransactionsListviewLoading(),
+                                    ],
+                                  );
+                                default:
+                                  return const Padding(
+                                    padding: EdgeInsets.all(mediumSize),
+                                    child: Text(
+                                      'An unknown error occured. Please reload the page.',
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  );
+                              }
+                            },
                           ),
                         ),
-                        rightButton: CustomAppbarButton(
-                          icon: Icons.tune,
-                          onTap: () {},
+                      ],
+                    );
+                  case ViewallViewExpenses():
+                    return Column(
+                      children: [
+                        ViewallHeader(
+                          isIos: isIos,
+                          isIncomesActive: false,
                         ),
-                      ),
-                    ),
-                    mediumSeparator,
-                    Padding(
-                      padding:
-                          const EdgeInsets.symmetric(horizontal: mediumSize),
-                      child: CustomSwitcher(
-                        isFirstButtonActive: viewState is ViewallViewIncomes,
-                        firstButtonOnTap: () {
-                          context.read<ViewallViewCubit>().switchToIncomes();
-                        },
-                        firstButtonText: 'Incomes',
-                        secondButtonOnTap: () {
-                          context.read<ViewallViewCubit>().switchToExpenses();
-                        },
-                        secondButtonText: 'Expenses',
-                      ),
-                    ),
-                    mediumSeparator,
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: mediumSize),
-                      child: ViewallChart(),
-                    ),
-                    smallSeparator,
-                    BlocBuilder<ViewallBloc, ViewallState>(
-                      builder: (context, blocState) {
-                        switch (blocState) {
-                          case ViewallLoaded()
-                              when viewState is ViewallViewIncomes &&
-                                  blocState.incomesDetails != null:
-                            return blocState
-                                    .incomesDetails!.incomesList.isNotEmpty
-                                ? TransactionsListview(
-                                    transactionsListCards: [
-                                      for (final incomeEntity in blocState
-                                          .incomesDetails!.incomesList)
-                                        TransactionsListCard(
-                                          transaction: incomeEntity,
-                                          deleteTransactionCallback: () {
-                                            context.read<ViewallBloc>().add(
-                                                  ViewallDeleteIncomeEvent(
-                                                    incomeEntity: incomeEntity,
-                                                  ),
-                                                );
-                                          },
+                        mediumSeparator,
+                        Expanded(
+                          child: BlocBuilder<ViewallBloc, ViewallState>(
+                            builder: (context, blocState) {
+                              switch (blocState) {
+                                case ViewallLoaded()
+                                    when blocState.expensesDetails != null:
+                                  return Column(
+                                    children: [
+                                      ViewallChart(
+                                        chart: blocState
+                                            .expensesDetails!.expensesChart,
+                                      ),
+                                      smallSeparator,
+                                      if (blocState.expensesDetails!
+                                          .expensesList.isNotEmpty)
+                                        TransactionsListview(
+                                          transactionsListCards: [
+                                            for (final expenseEntity
+                                                in blocState.expensesDetails!
+                                                    .expensesList)
+                                              TransactionsListCard(
+                                                transaction: expenseEntity,
+                                                deleteTransactionCallback: () {
+                                                  context
+                                                      .read<ViewallBloc>()
+                                                      .add(
+                                                        ViewallDeleteExpenseEvent(
+                                                          expenseEntity:
+                                                              expenseEntity,
+                                                        ),
+                                                      );
+                                                },
+                                              ),
+                                          ],
+                                        )
+                                      else
+                                        const Padding(
+                                          padding:
+                                              EdgeInsets.only(top: smallSize),
+                                          child: Text(
+                                            'There are no expenses yet.',
+                                          ),
                                         ),
                                     ],
-                                  )
-                                : const Padding(
-                                    padding: EdgeInsets.only(top: smallSize),
-                                    child: Text('There are no incomes yet.'),
                                   );
-                          case ViewallLoaded()
-                              when viewState is ViewallViewExpenses &&
-                                  blocState.expensesDetails != null:
-                            return blocState
-                                    .expensesDetails!.expensesList.isNotEmpty
-                                ? TransactionsListview(
-                                    transactionsListCards: [
-                                      for (final expenseEntity in blocState
-                                          .expensesDetails!.expensesList)
-                                        TransactionsListCard(
-                                          transaction: expenseEntity,
-                                          deleteTransactionCallback: () {
-                                            context.read<ViewallBloc>().add(
-                                                  ViewallDeleteExpenseEvent(
-                                                    expenseEntity:
-                                                        expenseEntity,
-                                                  ),
-                                                );
-                                          },
-                                        ),
+                                case ViewallLoaded()
+                                    when blocState.expensesDetails == null:
+                                  return const Padding(
+                                    padding: EdgeInsets.all(mediumSize),
+                                    child: Text(
+                                      'Unable to fetch data. Please reload the page.',
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  );
+                                case ViewallLoading():
+                                  return const Column(
+                                    children: [
+                                      ViewallChartLoading(),
+                                      smallSeparator,
+                                      TransactionsListviewLoading(),
                                     ],
-                                  )
-                                : const Padding(
-                                    padding: EdgeInsets.only(top: smallSize),
-                                    child: Text('There are no expenses yet.'),
                                   );
-                          case ViewallLoading():
-                            return const TransactionsListviewLoading();
-                          case ViewallLoaded()
-                              when blocState.incomesDetails == null ||
-                                  blocState.expensesDetails == null:
-                            return const Text(
-                              'Unable to fetch data. Please reload the page.',
-                            );
-                          default:
-                            return const Text('An unknown error occured.');
-                        }
-                      },
-                    ),
-                  ],
-                );
+                                default:
+                                  return const Padding(
+                                    padding: EdgeInsets.all(mediumSize),
+                                    child: Text(
+                                      'An unknown error occured. Please reload the page.',
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  );
+                              }
+                            },
+                          ),
+                        ),
+                      ],
+                    );
+                }
               },
             ),
           );
