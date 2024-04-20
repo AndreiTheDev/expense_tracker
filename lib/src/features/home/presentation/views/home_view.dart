48a6508e6b31362ad5e70b44e8a2d60f1d89b474
AndreiTheDev <andreithedevwork@gmail.com>
2024-04-20 22:18:00 +0300

    fix: remove unneded builder from homeview

--- lib/src/features/home/presentation/views/home_view.dart
+++ lib/src/features/home/presentation/views/home_view.dart
@@ -21,125 +21,119 @@ class HomeView extends StatelessWidget {
     const double appBarHeight = 90;
     context.read<AccountBloc>().add(const AccountFetchEvent());
 
-    return Builder(
-      builder: (context) {
-        return Scaffold(
-          body: BlocBuilder<AccountBloc, AccountState>(
-            builder: (context, state) {
-              if (state is AccountLoaded) {
-                final currentAccount = state.account;
-                return Column(
+    return Scaffold(
+      body: BlocBuilder<AccountBloc, AccountState>(
+        builder: (context, state) {
+          if (state is AccountLoaded) {
+            final currentAccount = state.account;
+            return Column(
+              children: [
+                xlSeparator,
+                const HomeAppBar(),
+                smallSeparator,
+                HomeCard(
+                  currentAccount: currentAccount,
+                ),
+                mediumSeparator,
+                const TransactionsListHeader(),
+                if (currentAccount.transactions.isEmpty)
+                  const Padding(
+                    padding: EdgeInsets.only(top: mediumSize),
+                    child: Text('There are no transactions yet.'),
+                  ),
+                if (currentAccount.transactions.isNotEmpty)
+                  TransactionsListview(
+                    transactionsListCards: [
+                      for (final transaction in currentAccount.transactions)
+                        TransactionsListCard(
+                          transaction: transaction,
+                          deleteTransactionCallback: () {
+                            context.read<AccountBloc>().add(
+                                  AccountDeleteTransactionEvent(
+                                    transactionEntity: transaction,
+                                  ),
+                                );
+                          },
+                        ),
+                    ],
+                  ),
+              ],
+            );
+          }
+          if (state is AccountLoading) {
+            return const Column(
+              children: [
+                xlSeparator,
+                HomeAppBar(),
+                smallSeparator,
+                HomeCardLoading(),
+                mediumSeparator,
+                Skeletonizer(
+                  child: TransactionsListHeader(),
+                ),
+                TransactionsListviewLoading(),
+              ],
+            );
+          }
+          return RefreshIndicator(
+            onRefresh: () {
+              return Future(
+                () =>
+                    context.read<AccountBloc>().add(const AccountFetchEvent()),
+              );
+            },
+            child: SingleChildScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: Column(
                   children: [
                     xlSeparator,
                     const HomeAppBar(),
-                    smallSeparator,
-                    HomeCard(
-                      currentAccount: currentAccount,
-                    ),
-                    mediumSeparator,
-                    const TransactionsListHeader(),
-                    if (currentAccount.transactions.isEmpty)
-                      const Padding(
-                        padding: EdgeInsets.only(top: mediumSize),
-                        child: Text('There are no transactions yet.'),
-                      ),
-                    if (currentAccount.transactions.isNotEmpty)
-                      TransactionsListview(
-                        transactionsListCards: [
-                          for (final transaction in currentAccount.transactions)
-                            TransactionsListCard(
-                              transaction: transaction,
-                              deleteTransactionCallback: () {
-                                context.read<AccountBloc>().add(
-                                      AccountDeleteTransactionEvent(
-                                        transactionEntity: transaction,
-                                      ),
-                                    );
-                              },
-                            ),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            state is AccountError
+                                ? state.message
+                                : 'An unknown error occured.',
+                          ),
+                          const Text(
+                            'Please slide down to refresh the page.',
+                          ),
                         ],
                       ),
-                  ],
-                );
-              }
-              if (state is AccountLoading) {
-                return const Column(
-                  children: [
-                    xlSeparator,
-                    HomeAppBar(),
-                    smallSeparator,
-                    HomeCardLoading(),
-                    mediumSeparator,
-                    Skeletonizer(
-                      child: TransactionsListHeader(),
                     ),
-                    TransactionsListviewLoading(),
                   ],
-                );
-              }
-              return RefreshIndicator(
-                onRefresh: () {
-                  return Future(
-                    () => context
-                        .read<AccountBloc>()
-                        .add(const AccountFetchEvent()),
-                  );
-                },
-                child: SingleChildScrollView(
-                  physics: const AlwaysScrollableScrollPhysics(),
-                  child: SizedBox(
-                    height: MediaQuery.of(context).size.height,
-                    child: Column(
-                      children: [
-                        xlSeparator,
-                        const HomeAppBar(),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                state is AccountError
-                                    ? state.message
-                                    : 'An unknown error occured.',
-                              ),
-                              const Text(
-                                'Please slide down to refresh the page.',
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
                 ),
-              );
-            },
-          ),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerDocked,
-          floatingActionButton: FloatingActionButton(
-            onPressed: () async {
-              await context.pushNamed(AppRoutes.addTransactions.name);
-            },
-            shape: const CircleBorder(),
-            child: Container(
-              height: double.maxFinite,
-              width: double.maxFinite,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: buttonsGradientReversed,
-              ),
-              child: const Icon(
-                Icons.add,
-                size: largeSize,
               ),
             ),
+          );
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await context.pushNamed(AppRoutes.addTransactions.name);
+        },
+        shape: const CircleBorder(),
+        child: Container(
+          height: double.maxFinite,
+          width: double.maxFinite,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: buttonsGradientReversed,
           ),
-          bottomNavigationBar: const HomeBottomNavigationBar(
-            appBarHeight: appBarHeight,
+          child: const Icon(
+            Icons.add,
+            size: largeSize,
           ),
-        );
-      },
+        ),
+      ),
+      bottomNavigationBar: const HomeBottomNavigationBar(
+        appBarHeight: appBarHeight,
+      ),
     );
   }
 }
